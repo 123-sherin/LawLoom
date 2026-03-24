79598a2d2ac97962534d30368f54b1df39df56e9
123-sherin <23bca041@caias.in>
2026-03-24 17:21:14 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-  const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/lawDBmongodb+srv://lawloom-admin:Lawloom@2026@cluster0.5f7ol3x.mongodb.net/?appName=Cluster0");
+    const conn = await mongoose.connect("mongodb+srv://lawloom-admin:2h6EPCQFs7kRy9Fo@cluster0.5f7ol3x.mongodb.net/lawloom?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     console.log(`📦 Database Name: ${conn.connection.name}`);
   } catch (error) {
