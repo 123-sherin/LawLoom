216f9d36b8528ce701acfb1ceb8651fd3ef24aa5
123-sherin <23bca041@caias.in>
2026-03-24 15:59:10 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb://localhost:27017/lawDBmongodb+srv://lawloom-admin:Lawloom@2026@cluster0.5f7ol3x.mongodb.net/?appName=Cluster0");
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     console.log(`📦 Database Name: ${conn.connection.name}`);
   } catch (error) {
